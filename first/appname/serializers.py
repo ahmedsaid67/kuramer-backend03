6e435c1f40dc2dc9ee2c6b80e6e00a61aa3043f4
ahmedsaid67 <96987324+ahmedsaid67@users.noreply.github.com>
2023-12-07 20:55:52 +0300

    Update serializers.py

--- first/appname/serializers.py
+++ first/appname/serializers.py
@@ -1,47 +1,14 @@
-from rest_framework import serializers,exceptions
+from rest_framework import serializers
 from .models import Slider
-from django.contrib.auth import authenticate
-from django.utils.translation import gettext_lazy as _
+
 from django.contrib.auth.models import User
 
-class AuthTokenSerializer(serializers.Serializer):
-    username = serializers.CharField(
-        label=_("Username"),
-        write_only=True
-    )
-    password = serializers.CharField(
-        label=_("Password"),
-        style={'input_type': 'password'},
-        trim_whitespace=False,
-        write_only=True
-    )
-    token = serializers.CharField(
-        label=_("Token"),
-        read_only=True
-    )
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email')
 
-    def validate(self, attrs):
-        username = attrs.get('username')
-        password = attrs.get('password')
 
-        if username and password:
-            user = authenticate(request=self.context.get('request'),
-                                username=username, password=password)
-            # böyle bir kullanıcı var ise o kullanıcı nesnesi döner yoksa None döner.
-
-            if not user:
-                msg = _('Unable to log in with provided credentials.')
-                raise serializers.ValidationError(msg, code='authorization')
-            if not user.is_active:
-                raise exceptions.AuthenticationFailed('Hesap aktif değil.')
-            #if user.is_removed:
-            #    raise exceptions.AuthenticationFailed('Hesap askıya alınmış.')
-        else:
-            msg = _('Must include "username" and "password".')
-            raise serializers.ValidationError(msg, code='authorization')
-
-        attrs['user'] = user  # geçici veri saklamaya yarar.
-        return attrs
 
 
 class UsersSerializers(serializers.ModelSerializer):
@@ -95,7 +62,20 @@ class PersonellerSerializer(serializers.ModelSerializer):
         personel_turu = PersonelTuru.objects.get(id=personel_turu_id)
         return Persons.objects.create(personel_turu=personel_turu, **validated_data)
 
+    def update(self, instance, validated_data):
+        personel_turu_id = validated_data.get('personel_turu_id', instance.personel_turu_id)
+        personel_turu = PersonelTuru.objects.get(id=personel_turu_id)
 
+        instance.ad = validated_data.get('ad', instance.ad)
+        instance.soyad = validated_data.get('soyad', instance.soyad)
+        instance.unvan = validated_data.get('unvan', instance.unvan)
+        instance.personel_turu = personel_turu
+        instance.img = validated_data.get('img', instance.img)
+        instance.durum = validated_data.get('durum', instance.durum)
+        instance.is_removed = validated_data.get('is_removed', instance.is_removed)
+
+        instance.save()
+        return instance
 
 
 
