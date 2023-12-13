2820d5c6450d7da33bfd19d07cf0203618fad33d
ahmedsaid67 <96987324+ahmedsaid67@users.noreply.github.com>
2023-12-13 13:36:30 +0300

    Update urls.py

--- first/appname/urls.py
+++ first/appname/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from .views import SliderListView, MenuViewSet, MenuItemListCreateView, MenuItemByMenuView,\
     MenuItemDetailView, MenuSelectedItemList,CustomAuthToken,CheckToken,UserInfoView
 from rest_framework.routers import DefaultRouter
-from .views import PersonelTuruViewSet,PersonellerViewSet,PersonellerListView
+from .views import PersonelTuruViewSet,PersonellerViewSet,PersonelTuruListView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -36,8 +36,9 @@ urlpatterns = [
 
     #personeller
     path('', include(router_personel_turu.urls)),
+    path('personelturu-list/', PersonelTuruListView.as_view(), name='personelturu-list'),
     path('', include(router_personel.urls)),
-    path('personeller-list/', PersonellerListView.as_view(), name='personeller-list'),
+    
 
     #auth apileri
     path('token/', CustomAuthToken.as_view(), name='api-token'),
